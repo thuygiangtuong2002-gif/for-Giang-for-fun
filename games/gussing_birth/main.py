12b6b8e738f8e6ea338993467d8a0f9088f04852
kunbaog8 <3b017096@gm.student.ncut.edu.tw>
2026-06-04 21:01:17 +0800

    修正了一些可能會有bug的地方

--- games/gussing_birth/main.py
+++ games/gussing_birth/main.py
@@ -1,3 +1,6 @@
+
+# 結構分為「主選單」「遊戲函式」「main 入口」結構化，好擴充
+
 def play_game():
 
     ANS = "2002/08/01"
@@ -6,22 +9,26 @@ def play_game():
     print("您好，進入猜生日遊戲")
     
 
-    
     for i in range(MAX_TRIES):
         print(f"請問Thùy Giang的生日是多少？你有{MAX_TRIES-i}次機會可以猜喔")
-        s = input()
+        
+        # 放入一個.strip()，以免使用者多輸入空格
+        s = input().strip()
 
         if s == ANS:
             print("猜對！我愛你")
             break
-        
-
+        else:
+            # 猜錯時給回饋，否則程式會直接進下一輪
+            print("猜錯了，再試試看")
     
+    # 如果迴圈全部跑完，還是沒有成功就給一個結束訊息，使用for...else:
+    else:
+        print(f"真可惜，答案是{ANS}")
 
 
 
     return 0
-
 
 
 
@@ -35,10 +42,9 @@ def main():
         print("2.不玩離開")
         
         options = input("輸入數字: ").strip()
-        # 這邊strip()一定要加嗎? 測試
+        # 如使用者輸入 " 1" 或 "1 "，有空白時，strip()會幫你清掉，程式就比較不容易出錯。
         if options == "1":
             play_game()
-            break
         elif options == "2":
             break
         else:
