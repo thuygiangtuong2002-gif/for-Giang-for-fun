bdbb1ba37293957551ba5158aa660b075f3cf63c
kunbaog8 <3b017096@gm.student.ncut.edu.tw>
2026-06-07 13:23:42 +0800

    修正Label一直出現的問題

--- games/gussing_birth/main.py
+++ games/gussing_birth/main.py
@@ -1,100 +1,42 @@
 import tkinter as tk
 
-
-
-
-# 結構分為「主選單」「遊戲函式」結構化，好擴充
-def play_game():
-
+def play_game(root):
     ANS = "2002/08/01"
-    MAX_TRIES = 3   
-    
-    print("")
+    MAX_TRIES = 3
+
     mylabel = tk.Label(root, text='您好，進入猜生日遊戲')
     mylabel.pack()
 
-    '''
-    for i in range(MAX_TRIES):
-        print(f"請問Thùy Giang的生日是多少？你有{MAX_TRIES-i}次機會可以猜喔")
-        
-        # 放入一個.strip()，以免使用者多輸入空格
-        s = input().strip()
-
-        if s == ANS:
-            print("猜對！我愛你")
-            break
-        else:
-            # 猜錯時給回饋，否則程式會直接進下一輪
-            print("猜錯了，再試試看")
-    
-    # 如果迴圈全部跑完，還是沒有成功就給一個結束訊息，使用for...else:
-    else:
-        print(f"真可惜，答案是{ANS}")
-
-
-    '''
-    
     return 0
 
 
 def main():
     root = tk.Tk()
-    root.title("Gussing game")
-    
-    # tk方法，取得螢幕大小
+    root.title("Guessing game")
+
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    # 我指定的軟體視窗大小
     window_width = 600
     window_height = 400
 
-    # 為了置中，給分給左右、上下相同大小。
     left = int((screen_width - window_width) / 2)
     top = int((screen_height - window_height) / 2)
 
     root.geometry(f'{window_width}x{window_height}+{left}+{top}')
-    
-    btn = tk.Button(root,
-                text='我是按鈕',
-                font=('Arial',30,'bold'),
-                padx=10,
-                pady=10,
-                activeforeground='#f00'
-                command = play_game
-              )
-    btn.pack()
 
-                                   
+    btn = tk.Button(
+        root,
+        text='我是按鈕',
+        font=('Arial', 30, 'bold'),
+        padx=10,
+        pady=10,
+        activeforeground='#f00',
+        command=lambda: play_game(root)
+    )
+    btn.pack()
 
     root.mainloop()
 
-    '''
-    while True:
-        print("1.玩")
-        print("2.不玩離開")
-        
-        options = input("輸入數字: ").strip()
-        # 如使用者輸入 " 1" 或 "1 "，有空白時，strip()會幫你清掉，程式就比較不容易出錯。
-        if options == "1":
-            play_game()
-        elif options == "2":
-            break
-        else:
-            print("輸入1或2喔！")
-        
-    
-    print("很棒！期待下次的遊戲喔")
-    
-    '''
 
 main()
-
-
-    
-
-
-
-
-
-
